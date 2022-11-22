b25869e94b00347f14d6bd75ed33a6f75d173681
Geet <geet1998patel@gmail.com>
2022-11-22 09:36:44 -0500

    More input fixing

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ from source import Dijkstra
 
 USAGE = """
 USAGE : python3 main.py <options>
-Example : python3 main.py -pq min-heap -n 1000 -d 1.6
-            runs dijkstra using min-heap priority queue 
+Example : python3 main.py -p min-heap -n 1000 -d 1.6 -r 5
+            runs dijkstra 5 times using min-heap priority queue 
             on a graph with 1000 nodes and approximate density 1.6
 """
 
 
 def default(default_):
     return default_ + ' [Default: %default]'
+
+
+def fillOption(option, strLead):
+    return option + " --" + "-" * (len(strLead) - len(option)) + " "
+
+
+def printOptions(options):
+    print("============ Options ============")
+    strLead = "Priority Queue"
+    print(fillOption("Priority Queue", strLead), options.priorityqueue)
+    print(fillOption("Nodes", strLead), options.nodes)
+    print(fillOption("Density", strLead), options.density)
+    print(fillOption("Runs", strLead), options.runs)
 
 
 def parseInput(argv):
@@ -24,21 +37,20 @@ def parseInput(argv):
                       choices=['min-heap', 'fib-heap'], default='min-heap')
     parser.add_option('-n', '--nodes', help=default('Nodes'), default=20)
     parser.add_option('-d', '--density', help=default('Approximate density'), default=1.5)
+    parser.add_option('-r', '--runs', help=default('Runs'), default=1)
 
     options, junk = parser.parse_args(argv)
     if len(junk) != 0:
         raise Exception('Command line input not understood: ' + str(junk))
 
-    print("======Starting======")
-    print("Priority Queue : \t\t", options.priorityqueue)
-    print("Nodes : \t\t", options.nodes)
-    print("Density : \t\t", options.density)
+    printOptions(options)
 
     pqClass = MinHeap.MinHeap if options.priorityqueue == "min-heap" else FibHeap.FibHeap
-    return (pqClass, options.nodes, options.density)
+    return (pqClass, options.nodes, options.density, int(options.runs))
 
 
 if __name__ == '__main__':
-    PriorityQueue, nodes, density = parseInput(sys.argv[1:])
-    dijkstra = Dijkstra.Dijkstra()
-    dijkstra.run(None, "source", PriorityQueue)
+    PriorityQueue, nodes, density, runs = parseInput(sys.argv[1:])
+    for i in range(0, runs):
+        print("============ Run {} ============".format(i))
+        dijkstra = Dijkstra.Dijkstra()
