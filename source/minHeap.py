8d948ce007ef4c6703d9fc976cc2008dfa17273f
Pankti-Nanavati <panktinanavati.pn@gmail.com>
2022-12-07 18:58:49 -0500

    min heap

--- source/minHeap.py
+++ source/minHeap.py
@@ -1,15 +1,45 @@
 from source.priorityQueue import PriorityQueue
 
-
 class MinHeap(PriorityQueue):
+    def __init__(self):
+        self.elements = []
+        self.size = 0
+
     def insert(self, key):
-        print("MinHeap insert({})".format(key))
+        self.elements.append(key)
+        self.size+=1
+        self.heapifyUpwards(self.size-1)
+        return 
+        # print("MinHeap insert({})".format(key))
 
     def findMin(self):
-        print("MinHeap findMin()")
+        return self.elements[0]
+        #print("MinHeap findMin()")
 
     def deleteMin(self):
-        print("MinHeap deleteMin()")
-
-    def decreaseKey(self, key):
-        print("MinHeap decreaseKey({})".format(key))
+        k = self.elements[0]
+        self.elements[0] = self.elements[-1]
+        del self.elements[-1]
+        self.size-=1
+        self.heapifyDownwards(0)
+        return k
+        #print("MinHeap deleteMin()")
+    
+    def heapifyUpwards(self, i):
+        while i != 0:
+            p = (i - 1) // 2
+            if self.elements[i][0] < self.elements[p][0]:
+                self.elements[i], self.elements[p] = self.elements[p], self.elements[i]
+            i = p
+    
+    def heapifyDownwards(self, i):
+        while i < self.size:
+            if 2 * (i + 1) >= self.size:
+                m = (2 * i) + 1
+            else:
+                m = (2 * i) + 1 if self.elements[(2 * i) + 1][0] < self.elements[2 * (i + 1)][0] else 2 * (i + 1)
+            if m>=self.size:
+                break
+            if self.elements[m][0] < self.elements[i][0]:
+                self.elements[i], self.elements[m] = self.elements[m], self.elements[i]
+            i = m
