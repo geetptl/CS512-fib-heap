5f5453ad146055ce313ae7db105bf01d9cffdc54
Geet <geet1998patel@gmail.com>
2022-12-07 18:14:18 -0500

    Edge weights added

--- source/dijkstra.py
+++ source/dijkstra.py
@@ -1,5 +1,8 @@
 class Dijkstra():
     def run(self, graph, PriorityQueue):
+        # How to access edge weights:
+        # for u, v, d in graph.edges(data=True):
+        #     print(d['weight'])
         print("Dijkstra run(nodes={}, edges={})".format(len(graph.nodes), len(graph.edges)))
         priorityQueue = PriorityQueue()
         for v in graph.nodes:
