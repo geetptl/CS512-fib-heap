2ab83eb08e37bdf6efd204f4d97f687b93507b24
darshee-m <darshee.m@somaiya.edu>
2022-12-07 19:04:10 -0500

    Dijkstra first pass

--- source/dijkstra.py
+++ source/dijkstra.py
@@ -1,6 +1,43 @@
+import time
+
 class Dijkstra():
+    def update(dist, pred, u, v):
+        if dist[u] + path_weight(graph, [u,v]) < dist[v]:
+            dist[v] = dist[u] + nx.path_weight(graph, [u,v])
+            pred[v] = u
+        return dist, pred
+    
+    def dijkstra(source):
+        dist = []
+        pred = []
+        for node in nodes(graph):
+            dist[node] = 10000
+            pred[node] = None
+            priorityQueue.insert(node)
+        dist[source] = 0
+
+        while len(minheap) != 0:
+            u = priorityQueue.deleteMin()
+            for v in neighbors(graph, u):
+                dist, pred = update(dist, pred, u, v)
+
+        return dist, pred
+
     def run(self, graph, PriorityQueue):
+        result_dist = []
+        result_pred = []
         print("Dijkstra run(nodes={}, edges={})".format(len(graph.nodes), len(graph.edges)))
         priorityQueue = PriorityQueue()
+        start_time = time.time()
         for v in graph.nodes:
-            priorityQueue.insert(v)
+            dist, pred = dijkstra(v)
+            result_dist[v] = dist
+            result_pred[v] = pred
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+
+
+            
+
