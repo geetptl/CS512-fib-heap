284c4aba90e48b9d38a2b6278a633e9bb9873820
Geet <geet1998patel@gmail.com>
2022-12-19 13:51:48 -0500

    Changes for testdata

--- source/dijkstraHandler.py
+++ source/dijkstraHandler.py
@@ -1,6 +1,5 @@
 import random
 
-import matplotlib.pyplot as plt
 import networkx as nx
 
 from source.dijkstra import Dijkstra
@@ -19,7 +18,7 @@ def isEqual(m_dist, f_dist, graph):
 class DijkstraHandler:
 
     @classmethod
-    def run(cls, nodes, edges, runs, PriorityQueue, priorityQueueType):
+    def run(cls, nodes, edges, runs, PriorityQueue, priorityQueueType, mute=False):
         for i in range(0, runs):
             print("Run {}".format(i + 1))
             graph = nx.gnm_random_graph(nodes, edges, seed=None, directed=False)
@@ -28,18 +27,14 @@ class DijkstraHandler:
                 weightedEdges.append((e[0], e[1], random.randint(a=0, b=100)))
                 graph.remove_edge(e[0], e[1])
             graph.add_weighted_edges_from(weightedEdges)
-            pos = nx.drawing.spring_layout(graph)
-            nx.draw(graph, pos, with_labels=True)
-            edge_labels = nx.get_edge_attributes(graph, "weight")
-            nx.draw_networkx_edge_labels(graph, pos, edge_labels)
-            plt.savefig("outputs/n_{}_e_{}_pq_{}_run_{}.png".format(nodes, edges, priorityQueueType, i))
-            plt.figure().clear()
+
             dijkstra = Dijkstra()
             if PriorityQueue is None:
                 m_time, m_dist, m_pred = dijkstra.run(graph, MinHeap)
                 f_time, f_dist, f_pred = dijkstra.run(graph, FibHeap)
-                print(isEqual(m_dist, f_dist, graph))
-                print("Minheap time", m_time)
-                print("Fibheap time", f_time)
+                if not mute:
+                    print(isEqual(m_dist, f_dist, graph))
+                    print("Minheap time", m_time)
+                    print("Fibheap time", f_time)
             else:
                 dijkstra.run(graph, PriorityQueue)
---
+++ test.py
@@ -0,0 +1,36 @@
+import time
+
+from source.dijkstraHandler import DijkstraHandler
+from source.fibHeap import FibHeap
+from source.minHeap import MinHeap
+
+if __name__ == '__main__':
+    densities = [d / 10 for d in range(2, 8)]
+    nodes = [10 ** i for i in range(1, 7)]
+    minheap_runtimes = {}
+    fibheap_runtimes = {}
+    for density_ in densities:
+        print("d={}".format(density_))
+        minheap_runtimes[density_] = {}
+        fibheap_runtimes[density_] = {}
+        for node_ in nodes:
+            print("n={}".format(node_))
+            edges_ = int(density_ * node_ * (node_ - 1) / 2)
+
+            t_minheap = time.process_time_ns()
+            DijkstraHandler.run(node_, edges_, 5, MinHeap, None, mute=True)
+            t_minheap = (time.process_time_ns() - t_minheap) * 1e-6
+            minheap_runtimes[density_][node_] = t_minheap
+            print("minheap runtime {}".format(t_minheap))
+
+            t_fibheap = time.process_time_ns()
+            DijkstraHandler.run(node_, edges_, 5, FibHeap, None, mute=True)
+            t_fibheap = (time.process_time_ns() - t_fibheap) * 1e-6
+            fibheap_runtimes[density_][node_] = t_fibheap
+            print("fibheap runtime {}".format(t_fibheap))
+
+            print("--------")
+        print("========")
+    print("================")
+    print("MinHeap : ", minheap_runtimes)
+    print("FibHeap : ", fibheap_runtimes)
