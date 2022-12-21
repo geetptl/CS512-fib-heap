acbb2beade79dd7c8bd084e0bd3bcdce0341e15b
Geet <geet1998patel@gmail.com>
2022-12-21 16:39:38 -0500

    Adding back visualisation

--- source/dijkstraHandler.py
+++ source/dijkstraHandler.py
@@ -1,5 +1,6 @@
 import random
 
+import matplotlib.pyplot as plt
 import networkx as nx
 
 from source.dijkstra import Dijkstra
@@ -27,6 +28,13 @@ class DijkstraHandler:
                 weightedEdges.append((e[0], e[1], random.randint(a=0, b=100)))
                 graph.remove_edge(e[0], e[1])
             graph.add_weighted_edges_from(weightedEdges)
+
+            pos = nx.drawing.spring_layout(graph)
+            nx.draw(graph, pos, with_labels=True)
+            edge_labels = nx.get_edge_attributes(graph, "weight")
+            nx.draw_networkx_edge_labels(graph, pos, edge_labels)
+            plt.savefig("outputs/n_{}_e_{}_pq_{}_run_{}.png".format(nodes, edges, priorityQueueType, i))
+            plt.figure().clear()
 
             dijkstra = Dijkstra()
             if PriorityQueue is None:
